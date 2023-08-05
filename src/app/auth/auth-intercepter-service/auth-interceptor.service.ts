b35799aca738af93a45883a9d75d783bdd5fb21c
Amine <mohamedaminesaidani7@gmail.com>
2023-08-05 19:58:43 +0100

    updating to milvus and sbert model

--- src/app/auth/auth-intercepter-service/auth-interceptor.service.ts
+++ src/app/auth/auth-intercepter-service/auth-interceptor.service.ts
@@ -19,31 +19,18 @@ export class AuthInterceptorService implements HttpInterceptor{
   constructor(private http:HttpClient) { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    console.log('intercepted request');
     const req = request.clone({
       setHeaders: {
         Authorization: `Bearer ${AuthInterceptorService.accessToken}`
       }
     });
-    console.log('resuest ...')
-    console.log(req)
-    console.log("access token after inctercepted ");
     console.log(AuthInterceptorService.accessToken);
     return next.handle(req).pipe(catchError((err: HttpErrorResponse) => {
-      console.log(err.status);
-      console.log('error');
-      console.log(err);
-      console.log(err.error);
-      if (err.status === 403 && !this.refresh) {
+      if (err.status === 401 && !this.refresh) {
         this.refresh = true;
-        console.log('rstatus')
-        console.log(err.status)
         return this.http.post('https://localhost:8443/api/auth/refresh', {}, {withCredentials: true}).pipe(
           switchMap((res: any) => {
-            console.log('refreshed')
             AuthInterceptorService.accessToken = res.token;
-            console.log('access token after refresh')
-            console.log(AuthInterceptorService.accessToken)
             return next.handle(request.clone({
               setHeaders: {
                 Authorization: `Bearer ${AuthInterceptorService.accessToken}`
